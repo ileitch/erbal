fc1bc2b7da1032bfb7d119a725d11c2bf83ff306
Ian Leitch <port001@gmail.com>
2010-12-30 11:37:19 +1100

    Set the buffer initial value to a SafeBuffer instance for versions of Rails that use it

--- lib/erbal/rails.rb
+++ lib/erbal/rails.rb
@@ -1,8 +1,21 @@
 require 'erbal'
 
-class ErbalTemplateHandler < ActionView::TemplateHandler
-  include ActionView::TemplateHandlers::Compilable
-  def compile(template)
-    ::Erbal.new("<% __in_erb_template=true %>#{template.source}", {:buffer => '@output_buffer'}).parse
+if ActiveSupport.const_defined?('SafeBuffer')
+
+  class ErbalTemplateHandler < ActionView::TemplateHandler
+    include ActionView::TemplateHandlers::Compilable
+    def compile(template)
+      ::Erbal.new("<% __in_erb_template=true %>#{template.source}", {:buffer => '@output_buffer', :buffer_initial_value => 'ActiveSupport::SafeBuffer.new'}).parse
+    end
   end
+
+else
+
+  class ErbalTemplateHandler < ActionView::TemplateHandler
+    include ActionView::TemplateHandlers::Compilable
+    def compile(template)
+      ::Erbal.new("<% __in_erb_template=true %>#{template.source}", {:buffer => '@output_buffer'}).parse
+    end
+  end
+
 end
