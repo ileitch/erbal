2178793242fffb85806080c5b661854bda5ffabb
Ian Leitch <port001@gmail.com>
2011-01-06 22:58:28 +1100

    Don't expect rails_xss plugin by default, add instructions to README for using Erbal with Rails 2.3 + rails_xss plugin.

--- lib/erbal/rails.rb
+++ lib/erbal/rails.rb
@@ -3,11 +3,10 @@ require 'erbal'
 
 class Erbal
   module Rails
-    class Rails_2_3_10_TemplateHandler < ActionView::TemplateHandler
+    class Rails_2_3_TemplateHandler < ActionView::TemplateHandler
       include ActionView::TemplateHandlers::Compilable
       def compile(template)
-        ::Erbal.new("<% __in_erb_template=true %>#{template.source}", {:buffer => '@output_buffer', :buffer_initial_value => 'ActiveSupport::SafeBuffer.new',
-          :safe_concat_method => 'safe_concat', :unsafe_concat_method => 'concat', :safe_concat_keyword => 'raw'}).parse
+        ::Erbal.new("<% __in_erb_template=true %>#{template.source}", {:buffer => '@output_buffer'}).parse
       end
     end
 
@@ -15,12 +14,9 @@ class Erbal
       raise "Sorry, this version of Erbal doesn't support Rails #{ActionPack::VERSION::MAJOR}.#{ActionPack::VERSION::MINOR}.#{ActionPack::VERSION::TINY}."
     end
 
-    def self.register_template_handler(handler_class)
-      ActionView::Template.register_template_handler :erb, handler_class
-    end
-
-    def self.register_template_handler_for_2_3_10
-      register_template_handler(Rails_2_3_10_TemplateHandler)
+    def self.register_template_handler_for_2_3
+      ActionView::Template.register_template_handler :erb, Rails_2_3_TemplateHandler
+      ActionView::Template.register_template_handler :rhtml, Rails_2_3_TemplateHandler
     end
   end
 end
@@ -29,12 +25,7 @@ case ActionPack::VERSION::MAJOR
 when 2
   case ActionPack::VERSION::MINOR
   when 3
-    case ActionPack::VERSION::TINY
-    when 10
-      Erbal::Rails.register_template_handler_for_2_3_10
-    else
-      Erbal::Rails.version_unsupported
-    end
+    Erbal::Rails.register_template_handler_for_2_3
   else
     Erbal::Rails.version_unsupported
   end
